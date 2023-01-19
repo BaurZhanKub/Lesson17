2f93732b03bab7d54bc1419d1146c6d2dda8d3fd
BaurZhanKub <84919873+BaurZhanKub@users.noreply.github.com>
2023-01-19 14:22:27 +0600

    First

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public interface Main {
+    public static void main(String[] args) {
+        String[] cocktailNames = {"Milc","bvery","Choco","Exotic"};
+        double[] cocktailsPrise = {4.6,3.7,7.7,6};
+
+    }
+}
