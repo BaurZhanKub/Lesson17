b8cdcd039ae8dcda220c2ca94fe8538cdb5dd9a4
BaurZhanKub <84919873+BaurZhanKub@users.noreply.github.com>
2023-01-19 14:37:47 +0600

    First start

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public interface Main {
     public static void main(String[] args) {
         String[] cocktailNames = {"Milc","bvery","Choco","Exotic"};
         double[] cocktailsPrise = {4.6,3.7,7.7,6};
+        printMenu(cocktailNames,cocktailsPrise);
 
+    }
+    static void printLine(String name, double prise){
+        String pattern =  "| %-20s - %-3S$";
+        String line = String.format(pattern, name , prise);
+        System.out.println(line);
+    }
+    static void  printMenu(String [] name, double[] price){
+        for (int i = 0; i < name.length ; i++) {
+            printLine(name[i],price[i]);
+        }
     }
 }
